8c758c905b510acfead362688dea064c9b6b36df
gayeon7877 <gayean01@gmail.com>
2023-11-26 01:12:40 +0900

    Feat:MainPage

---
+++ app/src/main/java/com/rup/feature/presentation/Main/MainRecyclerViewAdapter.kt
@@ -0,0 +1,39 @@
+package com.rup.feature.presentation.Main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rup.databinding.ItemMainBinding
+import com.rup.feature.presentation.Main.model.maindto
+
+class MainRecyclerViewAdapter(private var items: List<maindto>) : RecyclerView.Adapter<MainRecyclerViewAdapter.MainPageViewHolder>() {
+
+    class MainPageViewHolder(val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainPageViewHolder {
+        val binding = ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainPageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainPageViewHolder, position: Int) {
+        val item = items[position]
+
+        // Binding을 사용하여 데이터를 뷰에 연결
+        with(holder.binding) {
+            title.text = item.title
+            subscription.text = item.subscription
+            time.text = item.time
+            time2.text = item.amPm
+            headcount.text = item.headCount
+            location.text = item.location
+            price.text = item.price
+        }
+    }
+
+    fun updateData(newItems: List<maindto>) {
+        this.items = newItems
+        notifyDataSetChanged()  // Notify RecyclerView to update
+    }
+
+    override fun getItemCount(): Int = items.size
+}
