fba18305a6f8a47b79c17efdecb9d8f7086fd981
KimHyungSik <colrog968@gmail.com>
2023-11-26 00:18:19 +0900

    avatarView

---
+++ app/src/main/java/com/rup/core/view/AvatarView.kt
@@ -0,0 +1,45 @@
+package com.rup.core.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.bumptech.glide.Glide
+import com.rup.R
+import com.rup.databinding.AvatarViewBinding
+
+class AvatarView : FrameLayout {
+    constructor(context: Context) : super(context) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initView()
+    }
+
+    private val binding by lazy {
+        AvatarViewBinding.bind(
+            LayoutInflater.from(context).inflate(R.layout.avatar_view, this, false)
+        )
+    }
+
+    private fun initView() {
+        addView(binding.root)
+    }
+
+    fun setImageUrl(url: String){
+        Glide
+            .with(context)
+            .load(url)
+            .centerCrop()
+            .into(binding.avatar)
+    }
+}
