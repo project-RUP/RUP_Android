5cb49cc3d65ca933a1907daa59e6589c319d36ef
KimHyungSik <colrog968@gmail.com>
2023-11-26 02:51:10 +0900

    point recyclerview

--- app/src/main/java/com/rup/feature/presentation/point/PointActivity.kt
+++ app/src/main/java/com/rup/feature/presentation/point/PointActivity.kt
@@ -1,7 +1,9 @@
 package com.rup.feature.presentation.point
 
 import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rup.core.base.BaseBindingActivity
 import com.rup.databinding.ActivityPointBinding
@@ -17,11 +19,22 @@ class PointActivity : BaseBindingActivity<ActivityPointBinding, PointViewModel>(
     private val adapter by lazy {
         PointAdapter(emptyList())
     }
+
     override fun setup() {
         binding.pointHistory.layoutManager = LinearLayoutManager(this).apply {
             orientation = LinearLayoutManager.VERTICAL
         }
 
         binding.pointHistory.adapter = adapter
+
+        val decoration = DividerItemDecoration(this, LinearLayout.VERTICAL)
+        binding.pointHistory.addItemDecoration(decoration)
+
+        viewModel.pointHistory
+            .observe(this) {
+                adapter.setData(it)
+            }
+
+        viewModel.getPointHistory()
     }
 }
--- app/src/main/java/com/rup/feature/presentation/point/PointViewModel.kt
+++ app/src/main/java/com/rup/feature/presentation/point/PointViewModel.kt
@@ -1,6 +1,40 @@
 package com.rup.feature.presentation.point
 
+import androidx.lifecycle.MutableLiveData
 import com.rup.core.base.BaseViewModel
+import com.rup.feature.presentation.point.model.PointHistoryItem
 
-class PointViewModel: BaseViewModel() {
+class PointViewModel : BaseViewModel() {
+
+    private val _pointHistory = MutableLiveData(emptyList<PointHistoryItem>())
+    val pointHistory get() = _pointHistory
+
+    fun getPointHistory() {
+        val newPointHistoryItem = listOf(
+            PointHistoryItem(
+                "1",
+                "1",
+                "1",
+                "1",
+            ),
+            PointHistoryItem(
+                "2",
+                "2",
+                "2",
+                "2",
+            ),
+            PointHistoryItem(
+                "3",
+                "3",
+                "3",
+                "3",
+            ), PointHistoryItem(
+                "4",
+                "4",
+                "4",
+                "4",
+            )
+        )
+        _pointHistory.value = newPointHistoryItem
+    }
 }
