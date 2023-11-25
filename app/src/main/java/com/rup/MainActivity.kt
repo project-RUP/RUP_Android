43c19f10bacd2ba5bba32929a51c04f15d59119c
gayeon7877 <gayean01@gmail.com>
2023-11-25 20:09:28 +0900

    Feat:Base

--- app/src/main/java/com/rup/MainActivity.kt
+++ app/src/main/java/com/rup/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.rup
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.ViewModelProvider
-import com.rup.core.base.BaseActivity
 import com.rup.core.base.BaseBindingActivity
 import com.rup.databinding.ActivityMainBinding
 
-class MainActivity : BaseBindingActivity<ActivityMainBinding, MainVIewModel>() {
+class MainActivity : BaseBindingActivity<ActivityMainBinding, MainViewModel>() {
 
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
         get() = ActivityMainBinding::inflate
@@ -17,6 +13,6 @@ class MainActivity : BaseBindingActivity<ActivityMainBinding, MainVIewModel>() {
     override fun setup() {
     }
 
-    override val viewModel: MainVIewModel
-        get() = ViewModelProvider(this).get(MainVIewModel::class.java)
+    override val viewModel: MainViewModel
+        get() = ViewModelProvider(this).get(MainViewModel::class.java)
 }
--- app/src/main/java/com/rup/MainViewModel.kt
+++ app/src/main/java/com/rup/MainViewModel.kt
@@ -2,5 +2,5 @@ package com.rup
 
 import com.rup.core.base.BaseViewModel
 
-class MainVIewModel: BaseViewModel() {
+class MainViewModel:BaseViewModel() {
 }
