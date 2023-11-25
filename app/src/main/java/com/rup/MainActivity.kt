c878f320b131759fc32418f9f1e949943014a80e
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-11-25 18:10:00 +0900

    Del: import 삭제

--- app/src/main/java/com/rup/MainActivity.kt
+++ app/src/main/java/com/rup/MainActivity.kt
@@ -2,9 +2,6 @@ package com.rup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
