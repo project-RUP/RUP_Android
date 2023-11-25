aae360703e554bcc433924f316c0ac78b9c20609
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-11-26 01:14:13 +0900

    안쓰는 import 삭제

--- app/src/main/java/com/rup/MainViewModel.kt
+++ app/src/main/java/com/rup/MainViewModel.kt
@@ -2,7 +2,6 @@ package com.rup
 
 import androidx.lifecycle.viewModelScope
 import com.rup.core.base.BaseViewModel
-import com.rup.network.AppModule
 import kotlinx.coroutines.launch
 
 class MainViewModel : BaseViewModel() {
